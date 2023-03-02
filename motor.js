332f109159cf1ba7fbbe9fda7472f1e0d48b2c8b
bellais <>
2023-03-02 23:50:56 +0000

    Initial commit

---
+++ motor.js
@@ -0,0 +1,10 @@
+function calcularconsumo() {
+    var km, gas, consumo
+
+    km = document.getElementById('km').value
+    gas = document.getElementById('gas').value
+
+    consumo = km / gas
+
+    document.querySelector("h1").innerHTML = consumo + " KM/L"
+}
